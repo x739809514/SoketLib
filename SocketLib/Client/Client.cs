eccd79277798339660e3b06e1a2728595c3081bb
x739809514 <1612650023@qq.com>
2024-02-28 14:41:18 +0000

    ASync for client

--- SocketLib/Client/Client.cs
+++ SocketLib/Client/Client.cs
@@ -4,9 +4,62 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+class SendObj{
+    public Socket skt;
+    public EndPoint pt;
+}
+
+class ReceiveData{
+    public Socket skt;
+    public byte[] dataBytes;
+}
+
 class Client
 {
     static void Main(String[] args)
+    {
+        ASyncSendData();
+        Console.Read();
+    }
+
+#region ASync
+    static void ASyncSendData()
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+        EndPoint pt = new IPEndPoint(ipAddress, 12700);
+        Console.WriteLine("Client Start...");
+        // connection is only once
+        socket.BeginConnect(pt, new AsyncCallback(SendDataHandle), new SendObj(){skt = socket, pt = pt});
+
+    }
+
+    static void SendDataHandle(IAsyncResult result)
+    {
+        if(result.AsyncState is SendObj args)
+        {
+            args.skt.EndConnect(result);
+
+            byte[] dataRcv = new byte[1024];
+            args.skt.BeginReceive(dataRcv,0,1024,SocketFlags.None,new AsyncCallback(ReceiveDataHandle), new ReceiveData(){skt = args.skt,dataBytes = dataRcv});
+        }
+    }
+
+    static void ReceiveDataHandle(IAsyncResult result){
+        if(result.AsyncState is ReceiveData args)
+        {
+            int len = args.skt.EndReceive(result);
+
+            string rcvMsg = Encoding.UTF8.GetString(args.dataBytes, 0, len);
+            Console.WriteLine("Rcv Server Data: " + rcvMsg);
+
+            args.skt.BeginReceive(args.dataBytes,0,1024,SocketFlags.None,new AsyncCallback(ReceiveDataHandle), new ReceiveData(){skt = args.skt,dataBytes = args.dataBytes});
+        }
+    }
+#endregion
+
+#region Sync
+    static void SyncSendData()
     {
         Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
@@ -42,4 +95,5 @@ class Client
             }
         }
     }
+#endregion
 }
